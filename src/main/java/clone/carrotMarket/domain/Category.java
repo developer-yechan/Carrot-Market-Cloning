a5d6df2dae164b4963e91a882bdc70825a21c2c8
kimyechan <kyc5244@gmail.com>
2023-06-26 10:40:49 +0900

    중고물품 Category 추가

--- src/main/java/clone/carrotMarket/domain/Category.java
+++ src/main/java/clone/carrotMarket/domain/Category.java
@@ -1,7 +1,8 @@
 package clone.carrotMarket.domain;
 
 public enum Category {
-    Digital("디지털기기"), Life("생활/가전"), Interior("가구/인테리어");
+    Digital("디지털기기"), Life("생활/가전"), Interior("가구/인테리어"),
+    Food("생활/가공식품"), Plant("식물"), Other("기타 중고물품");
     private final String value;
 
 
